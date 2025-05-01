09b7ea5f2c8ba18cc3aa0747b92b7a19db2eb687
sofiane <sofiane@bib-batteries.fr>
2025-05-01 16:01:35 +0200

    chore: initial commit

---
+++ src/db.js
@@ -0,0 +1,22 @@
+import sqlite3 from 'sqlite3';
+const db = new sqlite3.Database('./database.sqlite');
+
+export const all = (sql, params = []) => {
+  return new Promise((resolve, reject) => {
+    db.all(sql, params, (err, rows) => {
+      if (err) reject(err);
+      else resolve(rows);
+    });
+  });
+};
+
+export const run = (sql, params = []) => {
+  return new Promise((resolve, reject) => {
+    db.run(sql, params, function (err) {
+      if (err) reject(err);
+      else resolve({ id: this.lastID });
+    });
+  });
+};
+
+export default db;
